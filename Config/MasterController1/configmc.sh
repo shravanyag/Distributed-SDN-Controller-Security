38c41aa10135357a226efb545118adfa42f2a157
shravanyag <shravanya.g17@gmail.com>
2019-03-27 20:15:52 +0530

    Create configmc.sh

---
+++ Config/MasterController1/configmc.sh
@@ -0,0 +1,5 @@
+#! /bin/sh
+ifconfig enp2s0 10.0.0.50 netmask 255.255.255.0
+ip addr add 10.0.0.51/24 dev enp2s0
+sysctl -w net.ipv4.conf.all.accept_redirects=0
+ip route add 20.0.0.0/24 via 10.0.0.5
